1a755f55767fe3551c9762b47cfb2bc224c22c99
Tiago Cruz <tiagocruz1993@outlook.com>
2023-04-24 15:47:19 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,21 +8,19 @@ import ScreenFunctions
 import TargetFunctions
 import AlgorithmFunctions
 
-rats_picture = "D:\\Rats-1.0\\images\\mouse.png"
-target_image = "D:\\Rats-1.0\\images\\cheese.png"
-
-population_size = 500
-
+rats_picture = "D:\\Rats-1.1\\images\\mouse_N.png"
+target_image = "D:\\Rats-1.1\\images\\cheese.png"
 screen_width = 800
 screen_height = 800
 
-not_ready = True
-current_time = 0
-rats_begin_time = 0
 gene_index = 0
-time_to_live = 8000
-genome_size = 200
 generation_count = 1
+
+genome_size = 200
+population_size = 500
+
+first = True
+
 previus_movement_time = time.time()
 
 pygame.init()
@@ -30,7 +28,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Genetic Algorithm")
 
 screen = ScreenFunctions.create_screen(screen_width, screen_height)
-target_filled_group = TargetFunctions.initialize_target(target_image, screen_width / 2, 100)
+target_filled_group = TargetFunctions.initialize_target(target_image, screen_width - 25, screen_height - 25)  # !HardCode! deveria poder ter posicionamneto dinamico. Pensa.
 
 rats_filled_groups = RatsFunctions.initialize_rats(population_size, rats_picture)
 AlgorithmFunctions.random_rats_genomes(rats_filled_groups, genome_size)
@@ -47,36 +45,27 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if not_ready:
+        if first:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 rats_start_position = RatsFunctions.set_rats_start_position(rats_filled_groups, pygame.mouse.get_pos())
-                rats_begin_time = pygame.time.get_ticks()
-                not_ready = False
+                first = False
 
-    current_time = pygame.time.get_ticks()
-
-    if not not_ready:
-        if ((current_time - rats_begin_time) < time_to_live) and (gene_index < genome_size):
+    if not first:
+        if gene_index < genome_size:
             for group in rats_filled_groups:
-                t = Thread(target=group.sprites()[0].move, args=(gene_index, screen, delta_time))
+                t = Thread(target=group.sprites()[0].move, args=(gene_index, screen, delta_time))#  delta_time não será sempre +ou- 0.85. Pensa.
                 t.start()
             gene_index += 1
-            t.join()
         else:
             ranked_rats = AlgorithmFunctions.ranking_rats(rats_filled_groups, target_filled_group)
             best_rats = AlgorithmFunctions.get_best_rats(ranked_rats)
             rats_new_gen = RatsFunctions.initialize_rats(population_size, rats_picture)
-
-            # settar distancia antiga
-            AlgorithmFunctions.set_previus_distances(ranked_rats, rats_new_gen)
-
             AlgorithmFunctions.new_genomes(best_rats, rats_new_gen)
             AlgorithmFunctions.kill_rats(rats_filled_groups)
             RatsFunctions.set_rats_start_position(rats_new_gen, rats_start_position)
             rats_filled_groups = rats_new_gen
             gene_index = 0
-            rats_begin_time = pygame.time.get_ticks()
-            print(f"GENERATION: {generation_count} BEST SCORE: {best_rats[0][1][0]} DISTANCE FROM TARGET: {best_rats[0][1][1]}")
+            print(f"GENERATION: {generation_count} DISTANCE FROM TARGET: {best_rats[0][1]}")
             print(f"=== NEXT GEN ===")
             generation_count += 1
 
