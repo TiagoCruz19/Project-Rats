d31b59c7c5f736cd7fba656df144a25ee1467ed3
Tiago Cruz <tiagocruz1993@outlook.com>
2023-04-24 15:48:09 -0300

    Update Rats.py

--- Rats.py
+++ Rats.py
@@ -7,67 +7,50 @@ class Rat(pygame.sprite.Sprite):
         super().__init__()
 
         self.genome = []
-        self.angle = 0
-        self.score = 0
         self.speed = 100
         self.position = None
-        self.aux_img = None
-        self.previus_distance_from_target = 99999
-        self.image = pygame.transform.scale(pygame.image.load(picture_path), [30, 40])
-        self.rect = self.image.get_rect()
-        self.set_position(self.rect.center)
-
-    def update(self, screen):
-        pass
+        self.image = None
+        self.rect = None
+        self.images = [pygame.transform.scale(pygame.image.load("D:\\Rats-1.1\\images\\mouse_N.png"), [30, 40]),
+                       pygame.transform.scale(pygame.image.load("D:\\Rats-1.1\\images\\mouse_S.png"), [30, 40]),
+                       pygame.transform.scale(pygame.image.load("D:\\Rats-1.1\\images\\mouse_E.png"), [30, 40]),
+                       pygame.transform.scale(pygame.image.load("D:\\Rats-1.1\\images\\mouse_W.png"), [30, 40])]
 
     def move(self, gene_index, screen, delta_time):
-
         if self.get_genome()[gene_index] == "1000":
-            if self.angle != 0:
-                self.rotate(screen, -self.angle)
-                self.angle = 0
-            else:
-                if "-y" not in self.verify_wall_colision(screen):
-                    self.rect.y -= round(self.speed * delta_time)
-                    self.set_position(
-                        [self.get_position()[0], (self.get_position()[1] - round(self.speed * delta_time))])
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+            self.rect.center = [self.get_position()[0], self.get_position()[1]]  # erro
+            if "-y" not in self.verify_wall_colision(screen):
+                self.rect.y -= round(self.speed * delta_time)
+                self.set_position([self.get_position()[0], (self.get_position()[1] - round(self.speed * delta_time))])
 
         elif self.get_genome()[gene_index] == "0100":
-            if self.angle != -180:
-                self.rotate(screen, (self.angle - 180))
-                self.angle = -180
-            else:
-                if "+y" not in self.verify_wall_colision(screen):
-                    self.rect.y += round(self.speed * delta_time)
-                    self.set_position(
-                        [self.get_position()[0], (self.get_position()[1] + round(self.speed * delta_time))])
+            self.image = self.images[1]
+            self.rect = self.image.get_rect()
+            self.rect.center = [self.get_position()[0], self.get_position()[1]]
+            if "+y" not in self.verify_wall_colision(screen):
+                self.rect.y += round(self.speed * delta_time)
+                self.set_position([self.get_position()[0], (self.get_position()[1] + round(self.speed * delta_time))])
 
         elif self.get_genome()[gene_index] == "0010":
-            if self.angle != -90:
-                self.rotate(screen, (self.angle - 90))
-                self.angle = -90
-            else:
-                if "+x" not in self.verify_wall_colision(screen):
-                    self.rect.x += round(self.speed * delta_time)
-                    self.set_position(
-                        [(self.get_position()[0] + round(self.speed * delta_time)), self.get_position()[1]])
+            self.image = self.images[2]
+            self.rect = self.image.get_rect()
+            self.rect.center = [self.get_position()[0], self.get_position()[1]]
+            if "+x" not in self.verify_wall_colision(screen):
+                self.rect.x += round(self.speed * delta_time)
+                self.set_position([(self.get_position()[0] + round(self.speed * delta_time)), self.get_position()[1]])
 
         elif self.get_genome()[gene_index] == "0001":
-            if self.angle != 90:
-                self.rotate(screen, 90)
-                self.angle = 90
-            else:
-                if "-x" not in self.verify_wall_colision(screen):
-                    self.rect.x -= round(self.speed * delta_time)
-                    self.set_position(
-                        [(self.get_position()[0] - round(self.speed * delta_time)), self.get_position()[1]])
+            self.image = self.images[3]
+            self.rect = self.image.get_rect()
+            self.rect.center = [self.get_position()[0], self.get_position()[1]]
+            if "-x" not in self.verify_wall_colision(screen):
+                self.rect.x -= round(self.speed * delta_time)
+                self.set_position([(self.get_position()[0] - round(self.speed * delta_time)), self.get_position()[1]])
 
-        self.blit_func(screen)
-
-    def rotate(self, _screen, angle):
-        self.image = pygame.transform.rotate(self.image, angle)
-        self.rect = self.image.get_rect()
-        self.rect.center = [self.get_position()[0], self.get_position()[1]]
+        if self.get_genome()[gene_index] != "0000":
+            self.blit_func(screen)
 
     def blit_func(self, screen):
         screen.blit(self.image, [self.get_position()[0] - self.image.get_width() / 2, self.get_position()[1] - self.image.get_height() / 2])
@@ -82,7 +65,6 @@ class Rat(pygame.sprite.Sprite):
             colision_str += "-x"
         elif self.rect.right >= _screen.get_width():
             colision_str += "+x"
-
         return colision_str
 
     def get_position(self):
@@ -91,20 +73,8 @@ class Rat(pygame.sprite.Sprite):
     def set_position(self, _position):
         self.position = _position
 
-    def get_score(self):
-        return self.score
-
-    def set_score(self, _score):
-        self.score = _score
-
     def get_genome(self):
         return self.genome
 
     def set_genome(self, _genome):
         self.genome = _genome
-
-    def get_previus_distance_from_target(self):
-        return self.previus_distance_from_target
-
-    def set_previus_distance_from_target(self, _previus_distance_from_target):
-        self.previus_distance_from_target = _previus_distance_from_target
