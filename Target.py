1a484bd9faf5b1210f87c7c85115db936bb9925f
Tiago Cruz <tiagocruz1993@outlook.com>
2024-01-02 10:27:55 -0300

    Update Target.py

--- Target.py
+++ Target.py
@@ -1,10 +1,16 @@
 import pygame
 
 
-class Target(pygame.sprite.Sprite):
+class create_target(pygame.sprite.Sprite):
 
-    def __init__(self, picture_path, pos_x, pos_y):
+    def __init__(self, picture_path, x, y):
+
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load(picture_path), (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.center = [pos_x, pos_y]
+        self.rect.center = [x, y]
+
+
+def draw_and_update_target_group(screen, target_group):
+    target_group.draw(screen)
+    target_group.update()
